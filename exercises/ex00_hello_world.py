68fc28d2df1d99e155cc38ce412324c73f989682
Lukas Dendrolivanos <dendro@unc.edu>
2022-08-24 23:01:54 -0400

    Progress on exercise 1

--- exercises/ex00_hello_world.py
+++ exercises/ex00_hello_world.py
@@ -1,8 +1,3 @@
-"""My first program for COMP110"""
-
-
+"""My first program for COMP110."""
 __author__ = "730565579"
-
-
-
 print("Hello, world.")
---
+++ exercises/ex01_chardle.py
@@ -0,0 +1,31 @@
+"""EX01 - Chardle - A cute step towards Wordle"""
+__author__ = "730565579"
+user_word: str = input("Enter a 5-character word: ")
+if len(user_word) != 5:
+    print("Error: Word must contain 5 characters")
+    exit()
+user_letter: str = input("Enter a single character: ")
+if len(user_letter) != 1:
+    print("Error: Character must be a single character")
+    exit()
+num_letter: int = 0
+print("Searching for " + user_letter + " in " + user_word)
+if user_letter == user_word[0]:
+    print(user_letter + " found at index 0")
+    num_letter = num_letter + 1
+if user_letter == user_word[1]:
+    print(user_letter + " found at index 1")
+    num_letter = num_letter + 1
+if user_letter == user_word[2]:
+    print(user_letter + " found at index 2")
+    num_letter = num_letter + 1
+if user_letter == user_word[3]:
+    print(user_letter + " found at index 3")
+    num_letter = num_letter + 1
+if user_letter == user_word[4]:
+    print(user_letter + " found at index 4")
+    num_letter = num_letter + 1
+if num_letter > 0:
+    print(str(num_letter) + " instances of " + user_letter + " found in " + user_word)
+else:
+    print("No instances of " + user_letter + " found in " + user_word)
